d9fc8e9eb1617e75eaa3e25c84ed490f6ee10904
Kartik <kartikey.sri98@gmail.com>
2021-08-21 15:19:42 +0530

    fix: keymappings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,9 +1,10 @@
 local map = require('utils').map;
 
+map('n', '<Space>', '<Nop>');
 vim.g.mapleader = ' ';
 
-map('n', '<Leader>/', ':set hlsearch<CR>'); -- <Space> + / to toggle highlight.
-map('n', '<Leader><Leader>', ':<BS><CR>'); -- <Space><Space> to clear command line.
+map('n', '<Leader>/', ':set hlsearch!<CR>'); -- <Space> + / to toggle highlight.
+map('n', '<Leader><Leader>', ':<BS>', {silent = false}); -- <Space><Space> to clear command line.
 map('n', '0', '^') -- 0 moves cursor to the first character in the line instead of the absolute beginning of the line.
 map('n', 'j', 'gj') -- Moving down on wrapped lines
 map('n', 'k', 'gk') -- Moving up on wrapped lines
