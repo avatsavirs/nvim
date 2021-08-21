94a106acf3adab2389704f7f98d5cc5a9ad52620
Kartik <kartikey.sri98@gmail.com>
2021-08-21 08:37:05 +0530

    add: keymappings

---
+++ lua/keymappings.lua
@@ -0,0 +1,13 @@
+local map = require('utils').map;
+
+vim.g.mapleader = ' ';
+
+map('n', '<Leader>/', ':set hlsearch<CR>'); -- <Space> + / to toggle highlight.
+map('n', '<Leader><Leader>', ':<BS><CR>'); -- <Space><Space> to clear command line.
+map('n', '0', '^') -- 0 moves cursor to the first character in the line instead of the absolute beginning of the line.
+map('n', 'j', 'gj') -- Moving down on wrapped lines
+map('n', 'k', 'gk') -- Moving up on wrapped lines
+map('v', '>', '>gv') -- Keep block visually highlighted on >
+map('v', '<', '<gv') -- Keep block visually highlighted on <
+map('c', '<Left>', '<Space><BS><Left>') -- Use arrow keys to move cursor instead of moving through wildmenu in command line
+map('c', '<Right>', '<Space><BS><Right>') -- Use arrow keys to move cursor instead of moving through wildmenu in command line
