63d1dd8d9ce31e6ca1be2e4747bff9752228bf3f
Kartik <kartikey.sri98@gmail.com>
2021-08-23 10:26:42 +0530

    add: refresh buffer when vim resumes after suspension

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_exec([[
 augroup RefreshAutogroup
   autocmd!
-  autocmd FocusGained,BufEnter,UIEnter * :checktime
+  autocmd FocusGained,BufEnter,UIEnter,VimResume * :checktime
 augroup END
 ]], true);
