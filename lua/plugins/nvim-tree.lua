7dc1d4961574e6985d42463a6d48c359de7124d7
Kartik <kartikey.sri98@gmail.com>
2021-10-07 07:19:50 +0530

    fix: nvim-tree setup after breaking changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,16 +1,12 @@
+local nvim_tree = require'nvim-tree'
 local map = require('utils').map;
 
 local nvimtree_options = {
-  width = 66,
   ignore = { ".git", "node_modules", ".cache", "language-servers", ".undo_history" },
-  auto_close = 1,
-  follow = 1,
   hide_dotfiles = 0,
   git_hl = 0,
   highlight_opened_files = 1,
   root_folder_modifier = ":t",
-  tab_open = 0,
-  lsp_diagnostics = 1,
   indent_markers = 1,
   show_icons = {
     git = 0,
@@ -34,16 +30,26 @@ for opt, val in pairs(nvimtree_options) do
   vim.g["nvim_tree_" .. opt] = val
 end
 
-
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback;
-vim.g.nvim_tree_bindings = {
-  { key = "l", cb = tree_cb("edit") },
-  { key = { "h", "<BS>" }, cb = tree_cb("close_node") },
-  { key = "yy", cb = tree_cb("copy") },
-  { key = "x", cb = tree_cb("cut") },
-  { key = "dd", cb = tree_cb("remove") },
-  { key = "}", cb = tree_cb("next_sibling") },
-  { key = "{", cb = tree_cb("prev_sibling") },
-}
+
+nvim_tree.setup({
+  auto_close = false,
+  open_on_tab = false,
+  lsp_diagnostics = true,
+  view = {
+    width = 65,
+    mappings = {
+      list = {
+        { key = "l", cb = tree_cb("edit") },
+        { key = { "h", "<BS>" }, cb = tree_cb("close_node") },
+        { key = "yy", cb = tree_cb("copy") },
+        { key = "x", cb = tree_cb("cut") },
+        { key = "dd", cb = tree_cb("remove") },
+        { key = "}", cb = tree_cb("next_sibling") },
+        { key = "{", cb = tree_cb("prev_sibling") },
+      }
+    }
+  }
+})
 
 map('n', '<Leader>e', ':NvimTreeToggle<CR>');
