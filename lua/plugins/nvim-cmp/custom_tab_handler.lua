4b5063e05b832260bc10da3326fae9564019328a
Kartik <kartikey.sri98@gmail.com>
2021-10-22 09:01:58 +0530

    fix: nvim-cmp custom tab completion function

--- lua/plugins/nvim-cmp/custom_tab_handler.lua
+++ lua/plugins/nvim-cmp/custom_tab_handler.lua
@@ -1,30 +1,24 @@
+-- source: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
 local cmp = require('cmp');
+local luasnip = require("luasnip");
 
--- Return true if cursor is at the start of the line
--- or immediately after a blank space
-local check_back_space = function()
-  local col = vim.fn.col('.') - 1;
-  local current_line = vim.fn.getline('.');
-  local character_before_cursor = current_line:sub(col, col);
-  if col == 0 or character_before_cursor == ' ' then
-    return true
-  else
-    return false
-  end
+local has_words_before = function()
+  local line_no, col_no = unpack(vim.api.nvim_win_get_cursor(0))
+  local line_text = vim.api.nvim_buf_get_lines(0, line_no - 1, line_no, true)[1];
+  local char_before_cursor = line_text:sub(col_no, col_no);
+  return col_no ~= 0 and char_before_cursor:match("%s") == nil
 end
 
--- If menu is visible:  goes to next item in the list
--- Else if menu is not visible and cursor is after a character: opens up the menu
--- else works as normal tab i.e. inserts space.
 local function custom_tab_handler(fallback)
   if cmp.visible() then
     cmp.select_next_item();
-  elseif check_back_space() then
-    -- Fallback is normal Tab key behaviour i.e. insert space
-    fallback();
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump();
+  elseif has_words_before() then
+    cmp.complete();
   else
-    local trigger_menu_open = cmp.mapping.complete();
-    trigger_menu_open();
+    -- Fallback is normal Tab key behaviour i.e. insert Tab space
+    fallback();
   end
 end
 
