f3e323eecb4531f98994ad617bce2a638f8a0a24
Kartik <kartikey.sri98@gmail.com>
2021-10-31 13:07:53 +0530

    fix: nvim-autopairs breaking changes

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,6 +1,3 @@
 local map = require('utils').map;
 map('i', '<C-h>', '<BS>', {noremap = false}); -- <C-h> should act like backspace.
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true,
-});
 require('nvim-autopairs').setup();
