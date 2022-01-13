50a265e245605654426cf513029b4e51076cae22
Kartik <kartikey.sri98@gmail.com>
2022-01-13 09:34:02 +0530

    add: sumneko_lua diagnostics vim global var config

--- lua/plugins/nvim-lspconfig/server_configs/init.lua
+++ lua/plugins/nvim-lspconfig/server_configs/init.lua
@@ -2,5 +2,6 @@ local M = {};
 
 M.tsserver = require('plugins.nvim-lspconfig.server_configs.tsserver');
 M.efm = require('plugins.nvim-lspconfig.server_configs.efm');
+M.sumneko_lua = require('plugins.nvim-lspconfig.server_configs.sumneko_lua');
 
 return M;
