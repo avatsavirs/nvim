cd17002139e482f954bf71de68b3e3cd5922cab9
Kartik <kartikey.sri98@gmail.com>
2022-01-13 09:04:15 +0530

    update: lsp diagnostics commands

--- lua/plugins/nvim-lspconfig/on_attach.lua
+++ lua/plugins/nvim-lspconfig/on_attach.lua
@@ -20,9 +20,9 @@ local set_lsp_keybindings = function (_, buffer_number)
   vim.api.nvim_buf_set_keymap(buffer_number, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(buffer_number, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   vim.api.nvim_buf_set_keymap(buffer_number, 'n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buffer_number, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buffer_number, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buffer_number, 'n', ']]', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(buffer_number, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(buffer_number, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(buffer_number, 'n', ']]', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 end
 
 local set_diagnostic_symbols = function ()
