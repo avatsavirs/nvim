698df2fa01ee610797c94bb6bd8f9171b6f5d80a
Kartik <kartikey.sri98@gmail.com>
2022-01-17 21:20:54 +0530

    imporove(nvim-treesitter): add parser list to ensure_installed

---
+++ lua/plugins/nvim-treesitter/constants.lua
@@ -0,0 +1,24 @@
+local M = {};
+
+M.PARSER_INSTALL_LIST = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "dockerfile",
+  "dot",
+  "graphql",
+  "html",
+  "javascript",
+  "jsdoc",
+  "json",
+  "lua",
+  "markdown",
+  "python",
+  "regex",
+  "tsx",
+  "typescript",
+  "yaml",
+}
+
+return M;
