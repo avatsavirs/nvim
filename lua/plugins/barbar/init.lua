54af74ee879f55c0e4422f33427865e4826b52c6
Kartik <kartikey.sri98@gmail.com>
2022-01-17 11:57:29 +0530

    refactor: move keymappings to constants

--- lua/plugins/barbar/init.lua
+++ lua/plugins/barbar/init.lua
@@ -1,5 +1,10 @@
-require('plugins.barbar.keymappings').initialize();
 require('plugins.barbar.commands').initialize();
+local CONSTANTS = require('plugins.barbar.constants');
+local utils = require('utils');
+
+for _, value in ipairs(CONSTANTS.KEY_MAPPINGS) do
+  utils.map(value.mode, value.key_combination, value.command);
+end
 
 vim.g.bufferline = {
   animation = false,
