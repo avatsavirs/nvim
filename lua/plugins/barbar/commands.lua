6a89d18644ba95d6512bcdae43fb0a1aeffe6c69
Kartik <kartikey.sri98@gmail.com>
2022-01-17 12:35:16 +0530

    cleanup

--- lua/plugins/barbar/commands.lua
+++ lua/plugins/barbar/commands.lua
@@ -1,10 +1,10 @@
-local function initialize()
+local M = {};
+
+M.initialize = function ()
   vim.api.nvim_exec([[
-  command! BetterCloseAllButCurrent lua require('plugins.barbar.utils').betterCloseAllButCurrent()
-  command! BetterBufferClose lua require('plugins.barbar.utils').betterBufferClose()
+    command! BetterCloseAllButCurrent lua require('plugins.barbar.utils').betterCloseAllButCurrent()
+    command! BetterBufferClose lua require('plugins.barbar.utils').betterBufferClose()
   ]], true);
 end
 
-return {
-  initialize = initialize
-}
+return M;
