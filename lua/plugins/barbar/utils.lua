b74429b620ac4aa72c5c5401b3f1dfc27f9d1cc3
Kartik <kartikey.sri98@gmail.com>
2022-01-18 10:26:57 +0530

    refactor(barbar): betterBufferClose

--- lua/plugins/barbar/utils.lua
+++ lua/plugins/barbar/utils.lua
@@ -11,22 +11,22 @@ M.betterCloseAllButCurrent = function ()
   ]], true);
 end
 
-local get_number_of_listed_buffers = function ()
-  local count = 0;
-  for buffer = 1, vim.fn.bufnr('$') do
-    local is_listed = vim.fn.buflisted(buffer) == 1;
-    if is_listed then
-      count = count + 1;
+local get_listed_buffers = function ()
+  local all_bufs = vim.api.nvim_list_bufs();
+  local listed_buffers = {};
+  for _,value in ipairs(all_bufs) do
+    if(vim.fn.buflisted(value) == 1) then
+      table.insert(listed_buffers, value)
     end
-  end
-  return count;
+  end;
+  return listed_buffers;
 end
 
 M.betterBufferClose = function ()
   vim.api.nvim_exec([[
     execute "BufferClose"
   ]], true);
-  if(get_number_of_listed_buffers() == 1) then
+  if(#get_listed_buffers() == 1) then
     vim.api.nvim_exec([[
       execute "norm \<C-w>o"
     ]], true);
