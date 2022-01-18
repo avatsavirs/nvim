3309d0672895e1b0ec3482d19d3307a13aa13236
Kartik <kartikey.sri98@gmail.com>
2022-01-18 13:17:57 +0530

    refactor(utils): map functions

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,22 +2,23 @@ local utils = {};
 
 function utils.map(mode, key_combination, command, options)
 	local default_options = {noremap = true, silent = true}; -- All mappings are non-recursive and silent by default
-	if options then
-		for index, option in pairs(options) do
-			default_options[index] = option;
-		end
-	end
-	vim.api.nvim_set_keymap(mode, key_combination, command, default_options);
+	vim.api.nvim_set_keymap(
+    mode,
+    key_combination,
+    command,
+    vim.tbl_extend('force', default_options, options or {})
+  );
 end
 
 function utils.map_in_buffer(buffer_number, mode, key_combination, command, options)
 	local default_options = {noremap = true, silent = true}; -- All mappings are non-recursive and silent by default
-	if options then
-		for index, option in pairs(options) do
-			default_options[index] = option;
-		end
-	end
-  vim.api.nvim_buf_set_keymap(buffer_number, mode, key_combination, command, default_options);
+  vim.api.nvim_buf_set_keymap(
+    buffer_number,
+    mode,
+    key_combination,
+    command,
+    vim.tbl_extend('force', default_options, options or {})
+  );
 end
 
 function utils.set_option(option_name, option_value)
