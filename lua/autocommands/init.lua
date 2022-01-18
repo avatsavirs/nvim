442a009213fd2cf25d4ce37433b64872348caf61
Kartik <kartikey.sri98@gmail.com>
2022-01-18 15:00:20 +0530

    cleanup: use vim.cmd alias for vim.api.nvim_exec

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -1,7 +1,7 @@
 -- Refresh buffer on when terminal gains focus, buffer is entered, or vim is resumes after suspension
-vim.api.nvim_exec([[
+vim.cmd([[
 augroup RefreshAutogroup
   autocmd!
   autocmd FocusGained,BufEnter,UIEnter,VimResume * :checktime
 augroup END
-]], true);
+]]);
--- lua/plugins/barbar/commands.lua
+++ lua/plugins/barbar/commands.lua
@@ -1,10 +1,10 @@
 local M = {};
 
 M.initialize = function ()
-  vim.api.nvim_exec([[
+  vim.cmd([[
     command! BetterCloseAllButCurrent lua require('plugins.barbar.utils').betterCloseAllButCurrent()
     command! BetterBufferClose lua require('plugins.barbar.utils').betterBufferClose()
-  ]], true);
+  ]]);
 end
 
 return M;
