9dea8b3f396d61beab539a2eec7806e602bbc185
avatsavirs <kartikey.srivastava@healthify.co>
2022-11-01 10:28:25 +0530

    add(utils): comments

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,34 +2,36 @@ local utils = {};
 
 function utils.map(mode, key_combination, command, options)
 	local default_options = {noremap = true, silent = true}; -- All mappings are non-recursive and silent by default
+  local keymap_options = vim.tbl_extend('force', default_options, options or {}); -- merge default options with custom options args
 	vim.api.nvim_set_keymap(
     mode,
     key_combination,
     command,
-    vim.tbl_extend('force', default_options, options or {})
+    keymap_options
   );
 end
 
 function utils.map_in_buffer(buffer_number, mode, key_combination, command, options)
 	local default_options = {noremap = true, silent = true}; -- All mappings are non-recursive and silent by default
+  local keymap_options = vim.tbl_extend('force', default_options, options or {}); -- merge default options with custom options args
   vim.api.nvim_buf_set_keymap(
     buffer_number,
     mode,
     key_combination,
     command,
-    vim.tbl_extend('force', default_options, options or {})
+    keymap_options
   );
 end
 
 function utils.set_option(option_name, option_value)
 	if (type(option_value)=='function') then
-		vim.opt[option_name] = option_value(vim.opt[option_name]._value);
+		vim.opt[option_name] = option_value(vim.opt[option_name]._value); -- current value for `option_name` is passed to `option_value` callback function.
   else
 		vim.opt[option_name] = option_value;
 	end
 end
 
-utils.join_paths = function(...)
+function utils.join_paths(...)
   return table.concat({ ... }, '/')
 end
 
