1c3c798f951177b839d21f9a10ee75110f112a95
avatsavirs <kartikey.srivastava@healthify.co>
2023-02-25 01:15:06 +0530

    fix: convert numbered motion to jump

--- lua/keymappings/init.lua
+++ lua/keymappings/init.lua
@@ -6,11 +6,9 @@ vim.g.mapleader = ' ';
 map('n', '<Leader>/', ':set hlsearch!<CR>'); -- <Space> + / to toggle highlight.
 map('n', '<Leader><Leader>', ':<BS>', {silent = false}); -- <Space><Space> to clear command line.
 map('n', '0', '^'); -- 0 moves cursor to the first character in the line instead of the absolute beginning of the line.
-map('n', 'j', 'gj'); -- Moving down on wrapped lines
-map('n', 'k', 'gk'); -- Moving up on wrapped lines
 map('v', '>', '>gv'); -- Keep block visually highlighted on >
 map('v', '<', '<gv'); -- Keep block visually highlighted on <
 map('n', '<C-j>', '<C-e>'); -- Scroll down
 map('n', '<C-k>', '<C-y>'); -- Scroll up
-map('n', 'k', '(v:count > 2 ? "m\'" .. v:count : "") .. "k"', { expr = true }) -- Convert numbered up motion to jump
-map('n', 'j', '(v:count > 2 ? "m\'" .. v:count : "") .. "j"', { expr = true }) -- Convert numbered down motion to jump
+map('n', 'k', '(v:count > 2 ? "m\'" .. v:count : "") .. "gk"', { expr = true }) -- Convert numbered up motion to jump, g is for moving down wrapped lines,
+map('n', 'j', '(v:count > 2 ? "m\'" .. v:count : "") .. "gj"', { expr = true }) -- Convert numbered down motion to jump, g is for moving down wrapped lines,
