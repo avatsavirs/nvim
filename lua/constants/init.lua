879380adc6b3351194dadac9de9270d7b8930ca6
avatsavirs <kartikey.srivastava@healthify.co>
2023-02-25 12:06:50 +0530

    add: config constants

--- lua/constants/init.lua
+++ lua/constants/init.lua
@@ -1,5 +1,7 @@
 local CONSTANTS = {
-  CONFIG_BASE_PATH = vim.fn.stdpath('config')
-}
+  CONFIG_BASE_PATH = vim.fn.stdpath('config'),
+  CACH_BASE_PATH = vim.fn.stdpath('cache'),
+  DATA_BASE_PATH = vim.fn.stdpath('data'),
+};
 
 return CONSTANTS;
