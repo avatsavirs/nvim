7d9a6173bc358fc12d89ed127ac64a6f518f2f20
avatsavirs <kartikey.srivastava@healthify.co>
2023-02-25 12:24:03 +0530

    refactor: move options constants into root constants

--- lua/constants/init.lua
+++ lua/constants/init.lua
@@ -1,7 +1,10 @@
-local CONSTANTS = {
-  CONFIG_BASE_PATH = vim.fn.stdpath('config'),
-  CACH_BASE_PATH = vim.fn.stdpath('cache'),
-  DATA_BASE_PATH = vim.fn.stdpath('data'),
-};
+local utils = require('utils');
+local CONSTANTS = {};
+
+CONSTANTS.CONFIG_BASE_PATH = vim.fn.stdpath('config');
+CONSTANTS.CACH_BASE_PATH = vim.fn.stdpath('cache');
+CONSTANTS.DATA_BASE_PATH = vim.fn.stdpath('data');
+CONSTANTS.UNDO_DIR = utils.join_paths(CONSTANTS.CONFIG_BASE_PATH, '.undo_history');
+CONSTANTS.PACK_PATH = utils.join_paths(CONSTANTS.CONFIG_BASE_PATH, '.plugins');
 
 return CONSTANTS;
