ba771087d52260ce95db5fd43dc46c34664e1d4b
avatsavirs <kartikey.srivastava@healthify.co>
2023-02-25 11:47:15 +0530

    cleanup: remove RefershAutoGroup autocmd since it works out of the box

--- lua/autocommands/init.lua
+++
@@ -1,7 +0,0 @@
--- Refresh buffer on when terminal gains focus, buffer is entered, or vim is resumes after suspension
-vim.cmd([[
-augroup RefreshAutogroup
-  autocmd!
-  autocmd FocusGained,BufEnter,UIEnter,VimResume * :checktime
-augroup END
-]]);
