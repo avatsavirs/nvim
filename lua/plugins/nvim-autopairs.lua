b47f049270bcbede247774c204f19fb80da2d9a3
avatsavirs <kartikey.srivastava@healthify.co>
2023-09-12 13:17:27 +0530

    fix: nvim-autopairs <C-h> and <C-w> mapping

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,3 +1,4 @@
-local map = require('utils').map
-map('i', '<C-h>', '<BS>', { noremap = false }) -- <C-h> should act like backspace.
-require('nvim-autopairs').setup()
+require('nvim-autopairs').setup({
+  map_c_h = true,
+  map_c_w = true
+})
