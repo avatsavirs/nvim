4cf916f937141382aa4ca45ca617e691506716db
avatsavirs <kartikey.srivastava@healthify.co>
2023-09-12 17:29:42 +0530

    add: show cofirm message for closing unsaved buffer in betterBufferClose

--- lua/plugins/barbar/utils.lua
+++ lua/plugins/barbar/utils.lua
@@ -1,10 +1,15 @@
 local M = {}
 
+local get_open_window_count = function()
+  return vim.fn.winnr('$')
+end
+
 M.betterCloseAllButCurrent = function()
   vim.api.nvim_command('BufferCloseAllButCurrent')
   -- vim.fn.winnr returns the window number of the last window in the current tab
   -- This is same as the total number of the windows in the current tab.
-  if vim.fn.winnr('$') > 1 then
+  local open_window_count = get_open_window_count()
+  if open_window_count > 1 then
     vim.api.nvim_input('<C-w>o')
   end
 end
@@ -16,10 +21,29 @@ local get_listed_buffers = function()
   end, all_bufs)
 end
 
+local close_buffer_with_confirmation = function ()
+  local success, _ = pcall(vim.api.nvim_command, 'BufferClose')
+  if success then
+    return
+  end
+  local buf_name = vim.fn.expand('%:t')
+  local msg = "Save changes to \"" .. buf_name .. "\""
+  local choice = vim.fn.confirm(msg, "&Yes\n&No\n&Cancel", 1)
+  if choice == 1 then
+    vim.api.nvim_command('silent w')
+    vim.api.nvim_command('BufferClose')
+    return
+  elseif choice == 2 then
+    vim.api.nvim_command('BufferClose!')
+    return
+  end
+end
+
 M.betterBufferClose = function()
-  vim.api.nvim_command('BufferClose')
+  close_buffer_with_confirmation()
   local listed_buffers_count = #get_listed_buffers()
-  if listed_buffers_count == 1 then
+  local open_window_count = get_open_window_count()
+  if listed_buffers_count == 1 and open_window_count > 1 then
     vim.api.nvim_input('<C-w>o')
   end
 end
