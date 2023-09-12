92021fb4b6c65bba055c6c5e7e1a4faa5c7e55d3
avatsavirs <kartikey.srivastava@healthify.co>
2023-09-12 15:44:38 +0530

    clenaup

--- lua/plugins/barbar/init.lua
+++ lua/plugins/barbar/init.lua
@@ -12,20 +12,3 @@ barbar.setup({
   auto_hide = false,
   clickable = false,
 })
---[[
-vim.g.bufferline = {
-  animation = false,
-  auto_hide = false,
-  clickable = false,
-  closable = false,
-  icon_custom_colors = false,
-  icon_separator_active = '▎',
-  icon_separator_inactive = '▎',
-  icons = true,
-  letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-  maximum_length = 50,
-  maximum_padding = 4,
-  no_name_title = vim.NIL,
-  semantic_letters = true,
-  tabpages = true,
-} ]]
