71996a2d68830c1377d1bee6128463f31e474758
avatsavirs <kartikey.srivastava@healthify.co>
2023-09-12 15:44:35 +0530

    add: list buffers command

--- lua/plugins/barbar/commands.lua
+++ lua/plugins/barbar/commands.lua
@@ -4,6 +4,7 @@ M.initialize = function()
   vim.cmd([[
     command! BetterCloseAllButCurrent lua require('plugins.barbar.utils').betterCloseAllButCurrent()
     command! BetterBufferClose lua require('plugins.barbar.utils').betterBufferClose()
+    command! ListBuffers lua require('plugins.barbar.utils').listBuffers()
   ]])
 end
 
--- lua/plugins/barbar/utils.lua
+++ lua/plugins/barbar/utils.lua
@@ -23,4 +23,28 @@ M.betterBufferClose = function()
   end
 end
 
+M.listBuffers = function ()
+  -- Table to hold the list of currently opened buffers
+  local open_buffers = {}
+
+  -- Iterate over all tab pages
+  for _, tabnr in ipairs(vim.api.nvim_list_tabpages()) do
+    -- Iterate over all windows in the current tab page
+    for _, winnr in ipairs(vim.api.nvim_tabpage_list_wins(tabnr)) do
+      -- Get the buffer associated with the current window
+      local bufnr = vim.api.nvim_win_get_buf(winnr)
+      -- Get the buffer name
+      local buf_name = vim.api.nvim_buf_get_name(bufnr)
+      -- Store the buffer number and name in the table
+      open_buffers[bufnr] = buf_name
+    end
+  end
+
+  -- Print the list of currently opened buffers
+  for bufnr, buf_name in pairs(open_buffers) do
+    print("Buffer number: " .. bufnr .. ", Buffer name: " .. buf_name)
+  end
+end
+
+
 return M
