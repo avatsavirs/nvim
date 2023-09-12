9d1902d8f463c87fae506b916935c92a3a98bab1
avatsavirs <kartikey.srivastava@healthify.co>
2023-09-12 11:43:41 +0530

    update: barbar breaking changes

--- lua/plugins/barbar/init.lua
+++ lua/plugins/barbar/init.lua
@@ -1,3 +1,4 @@
+local barbar = require('barbar')
 require('plugins.barbar.commands').initialize()
 local CONSTANTS = require('plugins.barbar.constants')
 local utils = require('utils')
@@ -6,6 +7,12 @@ for _, value in ipairs(CONSTANTS.KEY_MAPPINGS) do
   utils.map(value.mode, value.key_combination, value.command)
 end
 
+barbar.setup({
+  animation = false,
+  auto_hide = false,
+  clickable = false,
+})
+--[[
 vim.g.bufferline = {
   animation = false,
   auto_hide = false,
@@ -21,4 +28,4 @@ vim.g.bufferline = {
   no_name_title = vim.NIL,
   semantic_letters = true,
   tabpages = true,
-}
+} ]]
