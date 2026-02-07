2c3ed58cba90f8f399b21f8be6fb57012e095b02
Kartikey Srivastava <kartikey@Kartikey-Srivastava.local>
2026-02-07 11:56:33 +0530

    fix: jump-list back on empty buffer

--- lua/keymappings/init.lua
+++ lua/keymappings/init.lua
@@ -1,7 +1,5 @@
 local map = require('utils').map
-local commands = require('keymappings.commands')
-
-commands.initialize()
+local keymappings_utils = require('keymappings.utils')
 
 map('n', '<Space>', '<Nop>')
 vim.g.mapleader = ' '
@@ -13,6 +11,6 @@ map('v', '>', '>gv') -- Keep block visually highlighted on >
 map('v', '<', '<gv') -- Keep block visually highlighted on <
 map('n', '<C-j>', '<C-e>') -- Scroll down
 map('n', '<C-k>', '<C-y>') -- Scroll up
-map('n', '<C-l>', ':OpenLastFile<CR>')
+map('n', '<C-o>', keymappings_utils.jumpBackWithStartupBufferCleanup)
 map('n', 'k', '(v:count > 2 ? "m\'" .. v:count : "") .. "gk"', { expr = true }) -- Convert numbered up motion to jump, g is for moving down wrapped lines,
 map('n', 'j', '(v:count > 2 ? "m\'" .. v:count : "") .. "gj"', { expr = true }) -- Convert numbered down motion to jump, g is for moving down wrapped lines,
--- lua/keymappings/utils.lua
+++ lua/keymappings/utils.lua
@@ -1,16 +1,35 @@
 local M = {}
 
-M.openLastFile = function()
-  local oldfiles = vim.v.oldfiles
-  if oldfiles and #oldfiles > 0 then
-    local most_recent_oldfile = oldfiles[1]
-    if most_recent_oldfile then
-      vim.cmd('edit ' .. vim.fn.fnameescape(most_recent_oldfile))
-    else
-      print('No recent files found')
-    end
-  else
-    print('No recent files found')
+local function is_empty_startup_buffer(buffer_number)
+  if not vim.api.nvim_buf_is_valid(buffer_number) then
+    return false
+  end
+
+  if vim.api.nvim_get_option_value('modified', { buf = buffer_number }) then
+    return false
+  end
+
+  if vim.api.nvim_get_option_value('buftype', { buf = buffer_number }) ~= '' then
+    return false
+  end
+
+  if vim.api.nvim_buf_get_name(buffer_number) ~= '' then
+    return false
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(buffer_number, 0, 2, false)
+  return #lines == 1 and lines[1] == ''
+end
+
+M.jumpBackWithStartupBufferCleanup = function()
+  local startup_buffer = vim.api.nvim_get_current_buf()
+  local should_cleanup_startup_buffer = is_empty_startup_buffer(startup_buffer)
+  local ctrl_o = vim.api.nvim_replace_termcodes('<C-o>', true, false, true)
+  vim.api.nvim_feedkeys(ctrl_o, 'nx', false)
+
+  if should_cleanup_startup_buffer and startup_buffer ~= vim.api.nvim_get_current_buf()
+      and vim.api.nvim_buf_is_valid(startup_buffer) and is_empty_startup_buffer(startup_buffer) then
+    pcall(vim.api.nvim_buf_delete, startup_buffer, { force = false })
   end
 end
 
