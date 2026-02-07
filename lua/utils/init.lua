6040b2721ce8e3266d9604094e175712dd51d0e4
Kartikey Srivastava <kartikey@Kartikey-Srivastava.local>
2026-02-07 06:54:16 +0530

    update: map_in_buffer util

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -8,8 +8,8 @@ end
 
 function utils.map_in_buffer(buffer_number, mode, key_combination, command, options)
   local default_options = { noremap = true, silent = true } -- All mappings are non-recursive and silent by default
-  local keymap_options = vim.tbl_extend('force', default_options, options or {}) -- merge default options with custom options args
-  vim.api.nvim_buf_set_keymap(buffer_number, mode, key_combination, command, keymap_options)
+  local keymap_options = vim.tbl_extend('force', default_options, options or {}, { buffer = buffer_number }) -- merge default options with custom options args
+  vim.keymap.set(mode, key_combination, command, keymap_options)
 end
 
 function utils.set_option(option_name, option_value)
